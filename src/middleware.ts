1f662b5c543b19ed02a0e90488113a0e586ef76e
BhatNishanthGanesh <nishanth.cs21@sahyadri.edu.in>
2024-02-17 20:24:57 +0530

    .

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,5 +3,5 @@ export { default } from "next-auth/middleware";
 // to protect matching routes
 
 export const config = {
-  matcher: ["/", "/owes", "/pay", "/savings", "/calculator", "/expense*","/monthlyexpenses"],
+  matcher: ["/", "/owes", "/pay", "/savings", "/calculator", "/expense","/expense/day","/monthlyexpenses"],
 };
